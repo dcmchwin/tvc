42e2bbb3664ff89367bd3b3158c07292ca27443c
David McHardy <david.mchardy@cambridgeconsultants.com>
2017-10-16 15:25:08 +0100

    initial commit of dev env spec files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+version = '0.0.0'
+
+setup(name='tvc',
+      version=version,
+      packages=[],
+      test_required=[],
+      entry_points={
+          'console_scripts': [
+          ],
+      }
+      )
